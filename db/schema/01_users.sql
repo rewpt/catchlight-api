885c03b0a26e61b4f788a8fe95a71fa09b0052e8
Brett Bilyk <brettbilyk@gmail.com>
2022-02-04 15:03:57 -0700

    Update Users table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,9 +5,9 @@ DROP TABLE IF EXISTS users CASCADE;
 --   CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' )
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY,
   email VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
-  password NOT NULL,
+  password VARCHAR(255),
   date_created DATE DEFAULT CURRENT_DATE
-)
+);

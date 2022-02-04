ae3ebac4269b929d3f667071294c7a92b39672d3
Brett Bilyk <brettbilyk@gmail.com>
2022-02-04 15:02:46 -0700

    Create example file for queries

---
+++ db/queries.js
@@ -0,0 +1,14 @@
+// const { pool } = require("../connect");
+
+// const getItems = function(req, res) {
+  
+//   pool.query("SELECT * FROM users;", async(error, results) => {
+//     if(error) { throw error }
+//     console.log(results.rows)
+//     await res.status(200).json(results.rows)
+//   })
+// };
+
+// module.exports = {
+//   getItems
+// }

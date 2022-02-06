e006d260b57067c4ee46c56d872e958415fb580b
Brett Bilyk <brettbilyk@gmail.com>
2022-02-06 11:13:24 -0700

    Create seeds for interactions table

---
+++ db/seeds/03_interactions.sql
@@ -0,0 +1,6 @@
+INSERT INTO interactions (user_id, media_id, watched, rating) VALUES (1, 1, true, 'like');
+INSERT INTO interactions (user_id, media_id, watched) VALUES (2, 1, false);
+INSERT INTO interactions (user_id, media_id, watched, rating) VALUES (3, 1, true, 'meh');
+INSERT INTO interactions (user_id, media_id, watched, rating) VALUES (2, 2, true, 'like');
+INSERT INTO interactions (user_id, media_id, watched, rating) VALUES (3, 1, true, 'dislike');
+INSERT INTO interactions (user_id, media_id, watched, rating) VALUES (1, 3, true, 'like');

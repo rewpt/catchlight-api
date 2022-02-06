1045a74852e90a36b1ba5257897f0f0d13dafd2a
Brett Bilyk <brettbilyk@gmail.com>
2022-02-06 11:12:57 -0700

    Create seeds for users table

---
+++ db/seeds/01_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, email, password) VALUES ('Mildred', 'mildey77@gmail.com', '123');
+INSERT INTO users (name, email, password) VALUES ('Karen', 'karen@gmail.com', '234');
+INSERT INTO users (name, email, password) VALUES ('Alan', 'alan@fmail.com', '345');

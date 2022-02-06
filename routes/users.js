eb175fec70c1014b62c7cf8403e1e79be9f0c279
Brett Bilyk <brettbilyk@gmail.com>
2022-02-06 12:42:16 -0700

    Create initial root for users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
+// imports express
 const express = require('express');
+// imports express router
 const router = express.Router();
+// imports database connection
+const { pool } = require("../db");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  pool.query("SELECT * FROM users;", async(error, results) => {
+    if(error) { throw error }
+    console.log(results.rows)
+    await res.status(200).json(results.rows)
+  })
 });
 
+// exports route
 module.exports = router;

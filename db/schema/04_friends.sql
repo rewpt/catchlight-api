85a5b02359c172e23a1e7e2b458d9ff967cdba74
Brett Bilyk <brettbilyk@gmail.com>
2022-02-06 11:10:50 -0700

    Create schema for friends table

---
+++ db/schema/04_friends.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS friends CASCADE;
+
+CREATE TABLE friends (
+  id SERIAL PRIMARY KEY,
+  sending_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  recieving_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  friendship BOOLEAN DEFAULT FALSE,
+  friendship_pending BOOLEAN DEFAULT TRUE,
+  created DATE DEFAULT CURRENT_DATE
+);

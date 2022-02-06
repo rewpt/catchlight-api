6f4872e11e0230ddad412ba668ebe292154cc9be
Brett Bilyk <brettbilyk@gmail.com>
2022-02-06 12:41:52 -0700

    create initial route for root

---
+++ routes/media.js
@@ -0,0 +1,18 @@
+// imports express
+const express = require('express')
+// imports express router
+const router = express.Router();
+// imports database connection
+const { pool } = require("../db");
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  pool.query("SELECT * FROM media;", async(error, results) => {
+    if(error) { throw error }
+    console.log(results.rows)
+    await res.status(200).json(results.rows)
+  })
+});
+
+// exports route
+module.exports = router;

5e5798ab20ec1237fcc4468affff5f43fcd33634
Brett Bilyk <brettbilyk@gmail.com>
2022-02-06 11:08:01 -0700

    Update users schema
    Co-authored-by: ZP <rewpt@users.noreply.github.com>
    Co-authored-by: Marc Schulmeister <marcschul@users.noreply.github.com>

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,13 +1,10 @@
 DROP TABLE IF EXISTS users CASCADE;
 
--- CREATE EXTENSION citext;
--- CREATE DOMAIN email AS citext
---   CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' )
-
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL,
-  password VARCHAR(255),
-  date_created DATE DEFAULT CURRENT_DATE
+  password VARCHAR(255) NOT NULL,
+  created DATE DEFAULT CURRENT_DATE NOT NULL,
+  modified DATE DEFAULT CURRENT_DATE NOT NULL
 );

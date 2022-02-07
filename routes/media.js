1fee03d294fb85073851fa068ef2855e26ee0c3d
Marc Schulmeister <marcschulm@gmail.com>
2022-02-06 18:06:16 -0800

    init routes/media.js

--- routes/media.js
+++ routes/media.js
@@ -3,12 +3,61 @@ const router = require('express').Router();
 
 module.exports = db => {
 
-  /* GET users listing. */
-  router.get('/', function(req, res, next) {
-    db.query("SELECT * FROM media;", async(error, results) => {
+  // GET /api/media
+  // returns an array of media
+  router.get('/', async (req, res) => {
+    const query = 'SELECT * FROM media';
+    const { rows } = await db.query(query);
+    res.send(rows);
+  })
 
-    });
+  // GET /api/media/:id
+  // returns single media object
+  router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const params = [id];
+    const query = 'SELECT * FROM media WHERE id = $1';
+    const { rows } = await db.query(query, params);
+    res.send(rows);
   });
-  
+
+  // POST /api/media/:id
+  // Creates a new media
+  router.post('/:id', async (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const image = req.body.image;
+    const imdb_id = req.body.imdb_id;
+    const params = [title, description, image, imdb_id];
+    const query = 'INSERT INTO media (title, description, image, imdb_id) VALUES ($1, $2, $3, $4)';
+    await db.query(query, params);
+    res.send('created new media');
+  })
+
+  // PUT /api/media/:id
+  // Updates a media
+  router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const title = req.body.title;
+    const description = req.body.description;
+    const image = req.body.image;
+    const imdb_id = req.body.imdb_id;
+    const params = [title, description, image, imdb_id, id];
+    const query = 'UPDATE media SET title = $1, description = $2, image = $3, imdb_id = $4 WHERE id = $5';
+    await db.query(query, params);
+    res.send('updated media');
+  });
+
+
+  // DELETE /api/media/:id
+  // Destroys a media
+  router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const params = [id]
+    const query = 'DELETE FROM media WHERE id = $1';
+    await db.query(query, params);
+    res.send('destroyed media');
+  });
+
   return router;
 };

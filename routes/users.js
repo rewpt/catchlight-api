c2b49cce20e3c5921f52a26079990b57a75817dc
Brett Bilyk <brettbilyk@gmail.com>
2022-02-07 17:03:47 -0700

    create post to api/users/:id

--- routes/users.js
+++ routes/users.js
@@ -56,7 +56,32 @@ module.exports = db => {
 
   });
 
-  
+  router.put('/:id', async (req, res) => {
+    const date = new Date()
+    const userId = req.params.id
+    const extraQueryParams = [date, userId]
+
+    // takes req.body and outputs the values in an array to be used in the SET portion of the bellow query
+    const setValuesWithoutExtras = createSetString(req.body)
+
+    // takes the array created on line 65 and adds any additional options that need updating in the table
+    const setValuesWithExtras = addAdditionalSetOptions("modified", setValuesWithoutExtras)
+    const paramsArray = Object.values(req.body).concat(addonQueryParams)
+
+    const query = `
+      UPDATE users
+      SET ${setValuesWithExtras.toString()}
+      WHERE id = $${paramsArray.length};
+    `
+ 
+    try {
+      await db.query(query, paramsArray);
+      res.send("success: user updated");
+
+    } catch(error) {
+      res.send({"error": error.detail});
+    }
+  })
 
   return router;
 }

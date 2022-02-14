10edf9b3f7905a7985b582fe6642fe6f80b943db
Brett Bilyk <brettbilyk@gmail.com>
2022-02-14 16:34:55 -0700

    Add user profile seed data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,14 +1,14 @@
-INSERT INTO users (name, email, password) 
+INSERT INTO users (name, email, password, profile_picture) 
 VALUES 
-('Mildred Johns', 'presentation@gmail.com', '123'),
-('Karen Cole', 'karen@gmail.com', '123'),
-('Alan Peele', 'alan@fmail.com', '123'),
-('Brett Bilyk', 'brett@gmail.com', '666'),
-('Marc Schulmeister', 'marc@marc.com', '123'),
-('Zev Permac', 'zev@zev.com', '123'),
-('Jane Jones', 'jane@gmail.com', '123'),
-('Miles Walters', 'miles22@gmail.com', '123'),
-('Danny Devito', 'dannyd@gmail.com', '123'),
-('Ben Apples', 'ben@gmail.com', '123');
+('Mildred Johns', 'presentation@gmail.com', '123', 'https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'),
+('Karen Cole', 'karen@gmail.com', '123', 'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80'),
+('Alan Peele', 'alan@fmail.com', '123', 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80'),
+('Brett Bilyk', 'brett@gmail.com', '123', 'https://cdn.discordapp.com/attachments/867480158658756629/942843507096322128/thumb_retake.jpeg'),
+('Marc Schulmeister', 'marc@marc.com', '123', 'https://cdn.discordapp.com/attachments/867480158658756629/942845633985929266/20220205_121450.jpg'),
+('Zev Permac', 'zev@zev.com', '123', 'https://cdn.discordapp.com/attachments/921122273442803713/942842122405560371/AboutMeWebsite.jpeg'),
+('Jane Jones', 'jane@gmail.com', '123', 'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'),
+('Miles Walters', 'miles22@gmail.com', '123', 'https://images.unsplash.com/photo-1615813967515-e1838c1c5116?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2574&q=80'),
+('Danny Devito', 'dannyd@gmail.com', '123', 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Danny_DeVito_cropped_and_edited_for_brightness.jpg/1024px-Danny_DeVito_cropped_and_edited_for_brightness.jpg'),
+('Ben Apples', 'ben@gmail.com', '123', 'https://cdn.discordapp.com/attachments/867480158658756629/942842921932165150/apple_158989157.jpg');
 
 

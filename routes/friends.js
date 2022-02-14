b81238ff2ad706c2338d9746613c5d45a70a334b
rewpt <zpermack@gmail.com>
2022-02-14 14:01:36 -0400

    add working backend for posting a friend request

--- routes/friends.js
+++ routes/friends.js
@@ -1,11 +1,57 @@
 const router = require('express').Router();
+const { authenticateToken } = require('../middleware/authorization');
 
 module.exports = db => {
 
   /* GET home page. */
-  router.get('/', function(req, res, next) {
-    res.json({"we got this":"yep"})
+  router.post('/', authenticateToken, async function(req, res, next) {
+
+    
+    console.log('REQ.USER', req.user);
+    try {
+      //Get user2 id from email and user 1 from body
+      const query = `SELECT id FROM users WHERE email = $1`
+      queryParams1 = [req.body.email]
+      const { rows } = await db.query(query, queryParams1)
+      const userId1 = req.user.id;
+      const userId2 = rows[0].id;
+      
+      //Insert friendship into database
+      queryParams2= [userId1, userId2];
+      const query2= `INSERT INTO friends (sending_user_id, recieving_user_id) VALUES ($1, $2) returning *`
+      const friendReq = await db.query(query2, queryParams2);
+      console.log(friendReq)
+
+    } catch(error) {
+      res.send({"error": error.detail})
+    }
+    
   });
+
+  //ACCEPT FRIEND REQUEST
+  // router.post('/:id', authenticateToken, async function(req, res, next) {
+
+    
+  //   console.log('REQ.USER', req.user);
+  //   try {
+  //     //Get user2 id from email and user 1 from body
+  //     const query = `SELECT id FROM users WHERE email = $1`
+  //     queryParams1 = [req.body.email]
+  //     const { rows } = await db.query(query, queryParams1)
+  //     const userId1 = req.user.id;
+  //     const userId2 = rows[0].id;
+      
+  //     //Insert friendship into database
+  //     queryParams2= [userId1, userId2];
+  //     const query2= `INSERT INTO friends (sending_user_id, recieving_user_id) VALUES ($1, $2) returning *`
+  //     const friendReq = await db.query(query2, queryParams2);
+  //     console.log(friendReq)
+
+  //   } catch(error) {
+  //     res.send({"error": error.detail})
+  //   }
+    
+  // });
   
   return router;
 };

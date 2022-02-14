62916fbf0e92230e1ad157ff2d7882ea19310441
rewpt <zpermack@gmail.com>
2022-02-14 17:02:28 -0400

    create query for get friend notifications retrieves email, name, id

--- routes/friends.js
+++ routes/friends.js
@@ -6,8 +6,6 @@ module.exports = db => {
   /* GET home page. */
   router.post('/', authenticateToken, async function(req, res, next) {
 
-    
-    console.log('REQ.USER', req.user);
     try {
       //Get user2 id from email and user 1 from body
       const query = `SELECT id FROM users WHERE email = $1`
@@ -20,13 +18,30 @@ module.exports = db => {
       queryParams2= [userId1, userId2];
       const query2= `INSERT INTO friends (sending_user_id, recieving_user_id) VALUES ($1, $2) returning *`
       const friendReq = await db.query(query2, queryParams2);
-      console.log(friendReq)
+      console.log(friendReq.rows)
+      res.send('friend request made')
 
     } catch(error) {
       res.send({"error": error.detail})
     }
     
   });
+
+  //Get friend reqs for a specific user
+  router.get('/requests', authenticateToken, async function(req, res, next) {
+
+    try {
+      const query = `SELECT sending_user_id, users.name, users.email FROM friends JOIN users ON users.id = friends.sending_user_id WHERE friends.recieving_user_id = $1`
+      queryParams = [req.user.id];
+      const friendRequests = await db.query(query, queryParams)
+      res.send(friendRequests.rows)
+      console.log(friendRequests.rows)
+    } catch(error) {
+      res.send({"error": error.detail})
+    }
+    
+  });
+
 
   //ACCEPT FRIEND REQUEST
   // router.post('/:id', authenticateToken, async function(req, res, next) {

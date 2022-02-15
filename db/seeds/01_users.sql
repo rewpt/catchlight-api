99cc3dc3cdc6720676d4abac61884c07b3acbb67
Marc Schulmeister <marcschulm@gmail.com>
2022-02-14 17:23:06 -0800

    add comment describing password value

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
+-- passwords have been stored in hashed format
+-- password value = 'password'
 INSERT INTO users (name, email, password, profile_picture) 
 VALUES 
 ('Mildred Johns', 'presentation@gmail.com', '$2b$10$y3SJQS5SvhohnkeZ593eT.VjObqT.ZbfXBu/es4JA35CETZIQBpV6', 'https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'),

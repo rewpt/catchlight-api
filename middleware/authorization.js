1c9eda2e516a72603f4ff47cfbea8adee3ea941e
rewpt <zpermack@gmail.com>
2022-03-02 14:14:53 -0500

    replace hard coded env back with process.env

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -4,7 +4,7 @@ function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']; // Bearer TOKEN
   const token = authHeader && authHeader.split(' ')[1]; // checks authHeader is not null, if so Bearer = [0], Token = [1]
   if(token == null) return res.status(401).json({error: "Null token"});
-  jwt.verify(token, 'sdafsf32r3q2f3aff3wafaf3wfdawfw3af3wafaw3f3wafwfwa3f',(error, user) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(error, user) => {
     if(error) return res.status(403).json({error:error.message});
     req.user = user;
     next();

773558005423098084484f4acb85d35cd24858fb
rewpt <zpermack@gmail.com>
2022-03-02 12:42:55 -0500

    change client to db as export

--- db/index.js
+++ db/index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const { Client } = require('pg');
 
-const client = new Client({
+const db = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
+
+client.connect();
 
 module.exports = db;
